48f9285ee817642936402d0fc2608cb64c9021a3
Akshay Bakshi <bakshiakshay94@gmail.com>
2014-11-20 20:42:04 -0800

    Sends back message from body

--- run.py
+++ run.py
@@ -13,10 +13,15 @@ def hello_monkey():
     """Respond to incoming text by name."""
 
     from_number = request.values.get('From', None)
+    from_body = request.values.get('Body',None)
+    if from_body is None:
+        from_body = ""
+
     if from_number in callers:
-        message = callers[from_number] + ", thanks for the message!"
+        message = callers[from_number] + ", thanks for the message!" + "Your message was " + from_body
+
     else:
-        message = "Thanks for the message, human."
+        message = "Thanks for the message, human. Your message was: " + from_body
  
     resp = twilio.twiml.Response()
     resp.message(message)
