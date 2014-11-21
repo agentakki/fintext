27aaa7d9370de9b6094314125fde196994432871
Akshay Bakshi <bakshiakshay94@gmail.com>
2014-11-20 19:56:18 -0800

    Testing greetings

--- run.py
+++ run.py
@@ -2,10 +2,21 @@ from flask import Flask, request, redirect
 import twilio.twiml
  
 app = Flask(__name__)
+
+callers = {
+    "+13109808652" : "Akshay",
+    "+14803820676" : "Harrison"
+}
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
+    """Respond to incoming text by name."""
+
+    from_number = request.values.get('From',none)
+    if from_number in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Thanks for the message, human."
  
     resp = twilio.twiml.Response()
     resp.message("Hello, Mobile Monkey")
