3eb82ca67e416c18b328eee67cb14ef97b6c1bdf
Akshay Bakshi <bakshiakshay94@gmail.com>
2014-11-20 20:01:11 -0800

    Return of the greetings (Test)

--- run.py
+++ run.py
@@ -5,21 +5,22 @@ app = Flask(__name__)
 
 callers = {
     "+13109808652" : "Akshay",
-    "+14803820676" : "Harrison"
+    "+14803820676" : "Harrison",
 }
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming text by name."""
 
-    from_number = request.values.get('From',none)
+    from_number = request.values.get('From', None)
     if from_number in callers:
         message = callers[from_number] + ", thanks for the message!"
     else:
         message = "Thanks for the message, human."
  
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message(message)
+    
     return str(resp)
  
 if __name__ == "__main__":
